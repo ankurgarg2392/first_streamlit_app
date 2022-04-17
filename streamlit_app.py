3af9eafe9cdf68c44fbd6033036bb2553985994d
Ankur Garg <103864702+ankurgarg2392@users.noreply.github.com>
2022-04-17 10:20:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('My Mom\'s New Healthy Diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Poha')
+streamlit.text('Chilla')
+streamlit.text('Oats')
